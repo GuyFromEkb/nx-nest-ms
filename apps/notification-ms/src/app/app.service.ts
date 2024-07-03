92ff3d5c0cc4bbdb22182cd859893a30b9f0f1ca
GuyFromEkb <guyFromEkb@example.com>
2024-07-03 22:50:49 +0500

    init

--- apps/notification-ms/src/app/app.service.ts
+++ apps/notification-ms/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: "Hello API GATEWAY" };
+    return { message: "Hello FROM NOTIFICATION MICROSERVICE" };
   }
 }
